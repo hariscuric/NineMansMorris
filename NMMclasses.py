60caa13b59d9794716196d5183165bf670bc48a3
hariscuric <hariscuric123@gmail.com>
2024-03-02 14:13:06 +0100

    Developing NMMclasses.game.gameProgress function ongoing. Defining rules of game flow

--- NMMclasses.py
+++ NMMclasses.py
@@ -24,8 +24,13 @@ class gameStage(Enum):
 class gameTurn(Enum):
     WHITETURN = 0
     BLACKTURN = 1
-    WHITEEATBLACK = 3
-    BLACKEATWHITE = 4
+
+class possibleActions(Enum):
+    ADDSTONE = 0
+    MOVESTONE = 1
+    JUMPSTONE = 2
+    EATSTONE = 3
+
 
 
 
@@ -50,28 +55,6 @@ class field:
                     return True
         return False
     
-    def isMill(self, other1 : 'field', other2 : 'field') -> bool:
-        sameAngles = (self.angle == other1.angle and self.angle == other2.angle)
-        sameCircles = (self.circle == other1.circle and self.circle == other2.circle)
-        sameAngle01 = self.angle == other1.angle
-        sameAngle02 = self.angle == other2.angle
-        sameAngle12 = other1.angle == other2.angle
-        sameCircle01 = self.circle == other1.circle
-        sameCircle02 = self.circle == other2.circle
-        sameCircle12 = other1.circle == other2.circle
-        if (sameAngle01 and sameCircle01) or (sameAngle02 and sameCircle02) or (sameAngle12 and sameCircle12):
-            return False 
-        if sameAngles:
-            if self.angle in [0,2,4,6]:
-                return True
-        if sameCircles:
-            a = self.angle in [7,0,1] and other1.angle in [7,0,1] and other2.angle in [7,0,1]
-            b = self.angle in [1,2,3] and other1.angle in [1,2,3] and other2.angle in [1,2,3]
-            c = self.angle in [3,4,5] and other1.angle in [3,4,5] and other2.angle in [3,4,5]
-            d = self.angle in [5,6,7] and other1.angle in [5,6,7] and other2.angle in [5,6,7]
-            if a or b or c or d:
-                return True
-        return False
 
         
     
@@ -83,6 +66,9 @@ class board:
                 self.fields.append(field(i,j))
 
         self.message = ""
+        #list of 16 possible mills: 3 indexes represent field indexes from self.fields:
+        self.possibleMillIndexes = [[23,16,17],[17,18,19],[19,20,21],[21,22,23],[15,8,9],[9,10,11],[11,12,13],
+                              [13,14,15],[7,0,1],[1,2,3],[3,4,5],[5,6,7],[0,8,16],[2,10,18],[4,12,20],[6,14,22]]
 
     def addWhite(self, i, j) -> bool:
         if self.fields[i*8 + j].occupancy == fieldOccupancy(1) or self.fields[i*8 + j].occupancy == fieldOccupancy(2):
@@ -99,6 +85,23 @@ class board:
 
         self.fields[i*8 + j].occupancy = fieldOccupancy(2)
         return True
+    
+    def removeWhite(self, i, j) -> bool:
+        if self.fields[i*8 + j].occupancy == fieldOccupancy(0) or self.fields[i*8 + j].occupancy == fieldOccupancy(2):
+            self.message = "Field empty or black, couldn't remove white"
+            return False
+
+        self.fields[i*8 + j].occupancy = fieldOccupancy(0)
+        return True
+    
+    def removeBlack(self, i, j) -> bool:
+        if self.fields[i*8 + j].occupancy == fieldOccupancy(0) or self.fields[i*8 + j].occupancy == fieldOccupancy(1):
+            self.message = "Field empty or white, couldn't remove black"
+            return False
+
+        self.fields[i*8 + j].occupancy = fieldOccupancy(0)
+        return True
+
     
 
     def moveWhite(self, i1,j1,i2,j2) -> bool:
@@ -146,6 +149,45 @@ class board:
         self.fields[i1*8+j1].occupancy = fieldOccupancy(0)
         self.fields[i2*8+j2].occupancy = fieldOccupancy(2)
         return True
+    
+    def WhiteMills(self):
+        #returns list of 16 booleans for existenace of 16 mills form self.possibleMillIndexes
+        boolList = []
+        for i in self.possibleMillIndexes:
+            bool = True
+            for j in range(3):
+                if not self.fields[i[j]].occupancy.name == "WHITE":
+                    bool = False
+            boolList.append(bool)
+        return boolList
+    
+    def BlackMills(self):
+        #returns list of 16 booleans for existenace of 16 mills form self.possibleMillIndexes
+        boolList = []
+        for i in self.possibleMillIndexes:
+            bool = True
+            for j in range(3):
+                if not self.fields[i[j]].occupancy.name == "BLACK":
+                    bool = False
+            boolList.append(bool)
+        return boolList
+    
+    def WhiteCount(self) -> int:
+        m = 0
+        for i in self.fields:
+            if i.occupancy.name == "WHITE":
+                m = m+1
+        return m
+    
+    def BlackCount(self) -> int:
+        m = 0
+        for i in self.fields:
+            if i.occupancy.name == "BLACK":
+                m = m+1
+        return m
+
+
+
 
 
 
@@ -155,10 +197,121 @@ class game:
     def __init__(self) -> None:
         self.board = board()
         self.stage = gameStage(0)
+        self.initialStageCounter = 9
         self.turn = gameTurn(0)
-        self.message = "Initial stage, white to play"
+        self.action = possibleActions(0)
+        self.CurrentWhiteMills = self.board.WhiteMills()
+        self.CurrentBlackMills = self.board.BlackMills()
+        self.DoubleMill = False
+        self.message = "Initial stage, white to add stone."
 
-    def progressGame(self) -> bool:
+
+    def progressGame(self,field1 : field, field2=field(Circle(0),Angle(0))) -> bool:
+        a = self.CurrentWhiteMills
+        b = self.CurrentBlackMills
+
+        if self.stage.name == "FIRSTSTAGE":
+            if self.turn.name == "WHITETURN":
+                if self.action.name == "ADDSTONE":
+                    bool = self.board.addWhite(field1.circle.value, field1.angle.value)
+                    if bool == True:
+                        self.CurrentWhiteMills = self.board.WhiteMills()
+                        m = 0
+                        for i in range(16):
+                            if self.CurrentWhiteMills[i] == True and a[i] == False:
+                                m = m+1
+                        if m > 0:
+                            self.action = possibleActions(3)
+                        if m == 2:
+                            self.DoubleMill = True
+                        if m == 0:
+                            self.turn = gameTurn(1)
+                        return bool
+                    return bool
+                if self.action.name == "EATSTONE":
+                    bool = self.board.removeBlack(field1.circle.value, field1.angle.value)
+                    if bool == True:
+                        if self.DoubleMill == True:
+                            self.DoubleMill = False
+                            return bool
+                        self.turn = gameTurn(1)
+                        self.action = possibleActions(0)
+                        return bool
+                    return bool
+            if self.turn.name == "BLACKTURN":
+                if self.action.name == "ADDSTONE":
+                    bool = self.board.addBlack(field1.circle.value, field1.angle.value)
+                    if bool == True:
+                        self.initialStageCounter = self.initialStageCounter - 1
+                        if self.initialStageCounter == 0:
+                            self.stage = gameStage(1)
+                            self.action = possibleActions(1)
+                        self.CurrentBlackMills = self.board.BlackMills()
+                        m = 0
+                        for i in range(16):
+                            if self.CurrentBlackMills[i] == True and b[i] == False:
+                                m = m+1
+                        if m > 0:
+                            self.action = possibleActions(3)
+                        if m == 2:
+                            self.DoubleMill = True
+                        if m == 0:
+                            self.turn = gameTurn(0)
+                        return bool
+                    return bool
+                if self.action.name == "EATSTONE":
+                    bool = self.board.removeWhite(field1.circle.value, field1.angle.value)
+                    if bool == True:
+                        if self.DoubleMill == True:
+                            self.DoubleMill = False
+                            return bool
+                        self.turn = gameTurn(0)
+                        self.action = possibleActions(0)
+                        return bool
+                    return bool
+                
+        if self.stage.name == "SECONDSTAGE":
+            if self.turn.name == "WHITETURN":
+                if self.action.name == "MOVESTONE":
+                    bool = self.board.moveWhite(field1.circle.value, field1.angle.value,field2.circle.value, field2.angle.value)
+                    if bool == True:
+                        self.CurrentWhiteMills = self.board.WhiteMills()
+                        m = 0
+                        for i in range(16):
+                            if self.CurrentWhiteMills[i] == True and a[i] == False:
+                                m = m+1
+                        if m > 0:
+                            self.action = possibleActions(3)
+                        if m == 2:
+                            self.DoubleMill = True
+                        if m == 0:
+                            self.turn = gameTurn(1)
+                        return bool
+                    return bool
+                if self.action.name == "EATSTONE":
+                    bool = self.board.removeBlack(field1.circle.value, field1.angle.value)
+                    if bool == True:
+                        if self.DoubleMill == True:
+                            self.DoubleMill = False
+                            return bool
+                        if self.board.BlackCount()<3:
+                            self.stage = gameStage(2)
+                        self.turn = gameTurn(1)
+                        self.action = possibleActions(1)
+                        return bool
+                    return bool
+                        
+
+
+
+                
+
+
+
+
+
+
+
         return True
 
         
