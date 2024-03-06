0c49b26476442fdd08e94c43b73b5b12a744543a
hariscuric <hariscuric123@gmail.com>
2024-03-06 02:53:05 +0100

    computerPlayer developed, testing, debugging and optimization needed

--- computerPlayer.py
+++ computerPlayer.py
@@ -1,44 +1,99 @@
 from NMMclasses import *
+import random
+import copy
 
 class stoneColor(Enum):
     WHITE = 0
     BLACK = 1
 
-class player:
-    def __init__(self, game: game, stoneColor : stoneColor) -> None:
-        self.game = game
-        self.stoneColor = stoneColor
 
-    def computeHeuristicFunction(self) -> int:
-        if self.stoneColor == stoneColor.WHITE:
-            heuristicMeasure = self.game.board.WhiteCount()-self.game.board.BlackCount()
-            if self.game.board.WhiteCount() < 3:
-                heuristicMeasure = -7
-            if self.game.board.BlackCount() < 3:
-                heuristicMeasure = 7
-            if self.game.board.isWhiteStuck():
-                heuristicMeasure = -7
-            if self.game.board.isBlackStuck():
-                heuristicMeasure = 7
-            
-        if self.stoneColor == stoneColor.BLACK:
-            heuristicMeasure = self.game.board.BlackCount()-self.game.board.WhiteCount()
-            if self.game.board.WhiteCount() < 3:
-                heuristicMeasure = 7
-            if self.game.board.BlackCount() < 3:
-                heuristicMeasure = -7
-            if self.game.board.isWhiteStuck():
-                heuristicMeasure = 7
-            if self.game.board.isBlackStuck():
-                heuristicMeasure = -7
+
+def computeHeuristicFunction(game : game, stoneColor : stoneColor) -> int:
+    if stoneColor == stoneColor.WHITE:
+        heuristicMeasure = game.board.WhiteCount()-game.board.BlackCount()
+        if game.board.WhiteCount() < 3:
+            heuristicMeasure = -7
+        if game.board.BlackCount() < 3:
+            heuristicMeasure = 7
+        if game.board.isWhiteStuck():
+            heuristicMeasure = -7
+        if game.board.isBlackStuck():
+            heuristicMeasure = 7
         
-        return heuristicMeasure
+    if stoneColor == stoneColor.BLACK:
+        heuristicMeasure = game.board.BlackCount()-game.board.WhiteCount()
+        if game.board.WhiteCount() < 3:
+            heuristicMeasure = 7
+        if game.board.BlackCount() < 3:
+            heuristicMeasure = -7
+        if game.board.isWhiteStuck():
+            heuristicMeasure = 7
+        if game.board.isBlackStuck():
+            heuristicMeasure = -7
     
-    def computeOptimalAction(self, movesAhead):
-        #stopped here for the night
-        #to be continued
-        return True
+    return heuristicMeasure
+
+
+
+def computeOptimalAction(game : game, stonecolor : stoneColor, movesAhead : int):
+    
+    if movesAhead == 0:
+        return [computeHeuristicFunction(game,stonecolor), [field(Circle(0),Angle(0)),field(Circle(0),Angle(0))]]
+    
+    if game.outcome == gameOutcome.WHITEWINS:
+        if stonecolor == stoneColor.WHITE:
+            return [7 , [field(Circle(0),Angle(0)),field(Circle(0),Angle(0))]]
+        if stonecolor == stoneColor.BLACK:
+            return [-7 , [field(Circle(0),Angle(0)),field(Circle(0),Angle(0))]]
         
+    if game.outcome == gameOutcome.BLACKWINS:
+        if stonecolor == stoneColor.BLACK:
+            return [7 , [field(Circle(0),Angle(0)),field(Circle(0),Angle(0))]]
+        if stonecolor == stoneColor.WHITE:
+            return [-7 , [field(Circle(0),Angle(0)),field(Circle(0),Angle(0))]]
+        
+    gamecopy = copy.deepcopy(game)
+    actions = []
+    utilities = []
+
+
+    if gamecopy.action == possibleActions.ADDSTONE or gamecopy.action == possibleActions.EATSTONE:
+        for i in gamecopy.board.fields:
+            bool = gamecopy.progressGame(i)
+            gamecopy = copy.deepcopy(game)
+            if bool:
+                actions.append([i,field(Circle.INNER,Angle.NORTH)])
+
+    if gamecopy.action == possibleActions.MOVESTONE or gamecopy.action == possibleActions.JUMPSTONE:
+        for i in gamecopy.board.fields:
+            for j in gamecopy.board.fields:
+                bool = gamecopy.progressGame(i,j)
+                gamecopy = copy.deepcopy(game)
+            if bool:
+                actions.append([i,j])
+
+    for i in actions:
+        gamecopy.progressGame(i[0],i[1])
+        a = computeOptimalAction(gamecopy,stonecolor,movesAhead-1)
+        utility = a[0]
+        utilities.append(utility)
+        gamecopy = copy.deepcopy(game)
+
+    if (game.turn == gameTurn.WHITETURN and stonecolor == stoneColor.WHITE) or \
+    (game.turn == gameTurn.BLACKTURN and stonecolor == stoneColor.BLACK):
+        a = max(utilities)
+        indices = [i for i, x in enumerate(utilities) if x == a]
+        b = random.choice(indices)
+        return [a, actions[b]]
+    
+    if (game.turn == gameTurn.WHITETURN and stonecolor == stoneColor.BLACK) or \
+    (game.turn == gameTurn.BLACKTURN and stonecolor == stoneColor.WHITE):
+        a = min(utilities)
+        indices = [i for i, x in enumerate(utilities) if x == a]
+        b = random.choice(indices)
+        return [a, actions[b]]
+
+    
 
         
         
--- main.py
+++ main.py
@@ -1,55 +1,152 @@
 from NMMclasses import *
 from boardGraphics import *
+from computerPlayer import *
+
+
+def inputNumOfPlayers():
+    a = int(input("Input number of players (1 for single-player, 2 for multiplayer game): "))
+    if not (a==1 or a==2):
+        print("Input 1 or 2")
+        return inputNumOfPlayers()
+    return a
+
+def inputStoneColor():
+    a = int(input("Choose the color of the stone, 1 for white and 2 for black "))
+    if not (a==1 or a==2):
+        print("Input 1 or 2")
+        return inputStoneColor()
+    return a
+    
 
 
 def main():
-    a = game()
-    b = boardGraphics()
-    b.topText(a.message)
-    loop_condition = 1
-    while loop_condition:
-        b.drawGame(a)
-        currentAction = a.action
-        if a.outcome == gameOutcome.GAMEONGOING:
-            if currentAction == possibleActions.ADDSTONE or currentAction == possibleActions.EATSTONE:
-                field1 = b.clickedField()
-                bool = a.progressGame(field1)
-            if currentAction == possibleActions.MOVESTONE or currentAction == possibleActions.JUMPSTONE:
-                field1 = b.clickedField()
-                field2 = b.clickedField()
-                bool = a.progressGame(field1,field2)
-            if bool:
 
-                
-                if a.turn == gameTurn.WHITETURN:
-                    m1 = "WHITE TO PLAY, "
+    numOfPlayers = inputNumOfPlayers()
+
+    if numOfPlayers == 1:
+        stoneColorIndex = inputStoneColor()
+
+    #FOR MULTIPLAYER GAME (TWO PLAYERS):
+    if numOfPlayers == 2:
+        a = game()
+        b = boardGraphics()
+        b.topText(a.message)
+        loop_condition = 1
+        while loop_condition:
+            b.drawGame(a)
+            currentAction = a.action
+            if a.outcome == gameOutcome.GAMEONGOING:
+                if currentAction == possibleActions.ADDSTONE or currentAction == possibleActions.EATSTONE:
+                    field1 = b.clickedField()
+                    bool = a.progressGame(field1)
+                if currentAction == possibleActions.MOVESTONE or currentAction == possibleActions.JUMPSTONE:
+                    field1 = b.clickedField()
+                    field2 = b.clickedField()
+                    bool = a.progressGame(field1,field2)
+                if bool:
+
+                    
+                    if a.turn == gameTurn.WHITETURN:
+                        m1 = "WHITE TO PLAY, "
+                    else:
+                        m1 = "BLACK TO PLAY, "
+                    if a.action == possibleActions.ADDSTONE:
+                        m2 = "CLICK ON AN EMPTY FIELD TO ADD A STONE"
+                    elif a.action == possibleActions.EATSTONE:
+                        m2 = "CLICK ON AN OPPONENT'S STONE TO REMOVE IT"
+                    elif a.action == possibleActions.MOVESTONE:
+                        m2 = "CLICK ON A STONE AND AN ADJACENT FIELD TO MOVE IT TO"
+                    elif a.action == possibleActions.JUMPSTONE:
+                        m2 = "CLICK ON A STONE AND ANY FREE FIELD TO JUMP TO"
+                    b.topText(m1+m2)
                 else:
-                    m1 = "BLACK TO PLAY, "
-                if a.action == possibleActions.ADDSTONE:
-                    m2 = "CLICK ON AN EMPTY FIELD TO ADD A STONE"
-                elif a.action == possibleActions.EATSTONE:
-                    m2 = "CLICK ON AN OPPONENT'S STONE TO REMOVE IT"
-                elif a.action == possibleActions.MOVESTONE:
-                    m2 = "CLICK ON A STONE AND AN ADJACENT FIELD TO MOVE IT TO"
-                elif a.action == possibleActions.JUMPSTONE:
-                    m2 = "CLICK ON A STONE AND ANY FREE FIELD TO JUMP TO"
-                b.topText(m1+m2)
-            else:
-                b.topText(a.message)
+                    b.topText(a.message)
 
 
-        elif a.outcome==gameOutcome.WHITEWINS:
-            b.topText("WHITE WON. Press Esc to exit game")
-            key = b.window.getKey()
-            if key == "Escape":
-                loop_condition = 0
-                b.window.close()
-        elif a.outcome == gameOutcome.BLACKWINS:
-            b.topText("BLACK WON. Press Esc to exit game")
-            key = b.window.getKey()
-            if key == "Escape":
-                loop_condition = 0
-                b.window.close()
+            elif a.outcome==gameOutcome.WHITEWINS:
+                b.topText("WHITE WON. Press Esc to exit game")
+                key = b.window.getKey()
+                if key == "Escape":
+                    loop_condition = 0
+                    b.window.close()
+            elif a.outcome == gameOutcome.BLACKWINS:
+                b.topText("BLACK WON. Press Esc to exit game")
+                key = b.window.getKey()
+                if key == "Escape":
+                    loop_condition = 0
+                    b.window.close()
+
+
+
+    #FOR SINGLE PLAYER GAME:
+    if numOfPlayers == 1:
+        if stoneColorIndex == 1:
+            computerColor = stoneColor.BLACK
+        if stoneColorIndex ==2:
+            computerColor = stoneColor.WHITE
+        a = game()
+        b = boardGraphics()
+        b.topText(a.message)
+        loop_condition = 1
+        while loop_condition:
+            b.drawGame(a)
+            currentAction = a.action
+            if a.outcome == gameOutcome.GAMEONGOING:
+                if currentAction == possibleActions.ADDSTONE or currentAction == possibleActions.EATSTONE:
+                    if (a.turn == gameTurn.WHITETURN and computerColor == stoneColor.WHITE) or \
+                    (a.turn == gameTurn.BLACKTURN and computerColor == stoneColor.BLACK):
+                        b.topText("COMPUTER IS THINKING...")
+                        computerDecision = computeOptimalAction(a, computerColor, 3)
+                        computerFieldClick = computerDecision[1]
+                        field1 = computerFieldClick[0]
+                    else:
+                        field1 = b.clickedField()
+                    bool = a.progressGame(field1)
+                if currentAction == possibleActions.MOVESTONE or currentAction == possibleActions.JUMPSTONE:
+                    if (a.turn == gameTurn.WHITETURN and computerColor == stoneColor.WHITE) or \
+                    (a.turn == gameTurn.BLACKTURN and computerColor == stoneColor.BLACK):
+                        b.topText("COMPUTER IS THINKING...")
+                        computerDecision = computeOptimalAction(a, computerColor, 3)
+                        computerFieldClick = computerDecision[1]
+                        field1 = computerFieldClick[0]
+                        field2 = computerFieldClick[1]
+                    else:
+                        field1 = b.clickedField()
+                        field2 = b.clickedField()
+                    bool = a.progressGame(field1,field2)
+                if bool:
+
+                    
+                    if a.turn == gameTurn.WHITETURN:
+                        m1 = "WHITE TO PLAY, "
+                    else:
+                        m1 = "BLACK TO PLAY, "
+                    if a.action == possibleActions.ADDSTONE:
+                        m2 = "CLICK ON AN EMPTY FIELD TO ADD A STONE"
+                    elif a.action == possibleActions.EATSTONE:
+                        m2 = "CLICK ON AN OPPONENT'S STONE TO REMOVE IT"
+                    elif a.action == possibleActions.MOVESTONE:
+                        m2 = "CLICK ON A STONE AND AN ADJACENT FIELD TO MOVE IT TO"
+                    elif a.action == possibleActions.JUMPSTONE:
+                        m2 = "CLICK ON A STONE AND ANY FREE FIELD TO JUMP TO"
+                    b.topText(m1+m2)
+                else:
+                    b.topText(a.message)
+
+
+            elif a.outcome==gameOutcome.WHITEWINS:
+                b.topText("WHITE WON. Press Esc to exit game")
+                key = b.window.getKey()
+                if key == "Escape":
+                    loop_condition = 0
+                    b.window.close()
+            elif a.outcome == gameOutcome.BLACKWINS:
+                b.topText("BLACK WON. Press Esc to exit game")
+                key = b.window.getKey()
+                if key == "Escape":
+                    loop_condition = 0
+                    b.window.close()
+
 
 
 
